659b7edae251e4db836561ed39a6960c6b1c2bb6
Apollo-Dev <dangdoan2206@gmail.com>
2023-12-15 23:49:32 +0700

    add mdx to treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
