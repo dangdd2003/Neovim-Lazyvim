ce077fe44a9d3a8cc77e26c1742b09834b4a53d9
Apollo-Dev <dangdoan2206@gmail.com>
2023-12-27 01:56:25 +0700

    use treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    enabled = false,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
 
@@ -12,20 +11,5 @@ return {
       })
       vim.treesitter.language.register("markdown", "mdx")
     end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    enabled = false,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    enabled = false,
-  },
-
-  {
-    "windwp/nvim-ts-autotag",
-    enabled = false,
   },
 }
