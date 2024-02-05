29a33c7543d2d10fddb4c8a7e725a9a2453d6028
Apollo-Dev <dangdoan2206@gmail.com>
2024-02-05 17:09:37 +0700

    use pwsh on Windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Ad any additional keymaps here
 local keymap = vim.keymap
+local Util = require("lazyvim.util")
 
 -- Copy all
 -- keymap.set("n", "<C-c>", "<cmd> %y+ <CR>", { desc = "Copy whole file" })
@@ -31,6 +32,20 @@ keymap.set("n", "<Leader>vx", "<Cmd>VimtexClean<CR>", { desc = "Vimtex Clean" })
 keymap.set("i", "?", "?<c-g>u")
 keymap.set("i", "/", "/<c-g>u")
 keymap.set("i", ":", ":<c-g>u")
+
+-- Git
+keymap.set("n", "<Leader>gb", "<Cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Git Line Blame" })
+
+-- Terminal (change default shell to pwsh in Windows)
+local terminal = function()
+  Util.terminal("pwsh", { cwd = Util.root() })
+end
+keymap.set("n", "<leader>ft", terminal, { desc = "Terminal (root dir)" })
+keymap.set("n", "<leader>fT", function()
+  Util.terminal("pwsh")
+end, { desc = "Terminal (cwd)" })
+keymap.set("n", "<c-/>", terminal, { desc = "Terminal (root dir)" })
+keymap.set("n", "<c-_>", terminal, { desc = "which_key_ignore" })
 
 -- Delete unused keymap
 vim.keymap.del("n", "<S-h>")
