122fc3efbb8b1cf589f0ee3e8d771eeb3b5b0fd5
Apollo-Dev <dangdoan2206@gmail.com>
2024-02-21 12:20:31 +0700

    small fix code

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- neovide setings
-if vim.g.neovide then
-  vim.o.guifont = "Hack Nerd Font:h11"
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_cursor_vfx_mode = "railgun"
-end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.shell = "pwsh"
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m]"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Neovide (if installed)
+if vim.g.neovide then
+  vim.o.guifont = "Hack Nerd Font:h11"
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
