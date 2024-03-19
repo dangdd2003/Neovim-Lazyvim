555b4201ab37ddfe2b3f0def7f3a0524a91b86d2
Apollo-Dev <dangdoan2206@gmail.com>
2024-03-20 01:04:17 +0700

    lsp for main working languages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,8 +8,90 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<c-k>", mode = "i", false }
     end,
+    ---@type lspconfig.options
+    ---@diagnostic disable-next-line: missing-fields
     opts = {
-      servers = { eslint = {} },
+      servers = {
+        eslint = {},
+        tsserver = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          single_file_support = false,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+        html = {},
+        lua_ls = {
+          single_file_support = true,
+          settings = {
+            Lua = {
+              workspaceWord = {
+                checkThirdParty = false,
+              },
+              completion = {
+                workspaceWord = true,
+                callSnippet = "Both",
+              },
+              diagnostics = {
+                disable = { "incomplete-signature-doc", "trailing-space" },
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
+              doc = {
+                privateName = { "^_" },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+              },
+              type = {
+                castNumberToInteger = true,
+              },
+            },
+          },
+        },
+      },
       setup = {
         eslint = function()
           require("lazyvim.util").lsp.on_attach(function(client)
@@ -34,7 +116,16 @@ return {
       jdtls = function(opts)
         local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
         local jvmArg = "-javaagent:" .. install_path .. "/lombok.jar"
-        table.insert(opts.cmd, "--jvm-arg=" .. jvmArg)
+        table.insert(opts.cmd, "--jvm-arg=" .. jvmArg) -- Project Lombok Linting
+        table.insert(opts, { -- Codelens
+          require("lazyvim.util").lsp.on_attach(function(_, buffer)
+            vim.lsp.codelens.refresh()
+            vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+              buffer = buffer,
+              callback = vim.lsp.codelens.refresh,
+            })
+          end),
+        })
         return opts
       end,
     },
