d9a8fd9ea240b6bd9659f9c122d8cd33c4f17ce7
Apollo-Dev <dangdoan2206@gmail.com>
2024-04-05 12:16:36 +0700

    UI

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -52,6 +52,7 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
+      -- transparent = false,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -104,6 +104,11 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
         border = "rounded",
         height = 0.75,
       },
