9181213e5ca4dd1cab2db4a6c85d706d9c91c5a1
Apollo-Dev <dangdoan2206@gmail.com>
2024-04-20 01:02:35 +0700

    telescope ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,14 +12,6 @@ return {
     },
     keys = {
       {
-        "<leader>fB",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.buffers()
-        end,
-        desc = "Buffers",
-      },
-      {
         "<leader>fp",
         function()
           require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
@@ -27,47 +19,111 @@ return {
         desc = "Find Plugin File",
       },
       {
-        "<leader>fb",
+        "fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+      {
+        "fb",
         function()
           local fb_actions = require("telescope").extensions.file_browser
           fb_actions.file_browser()
         end,
         desc = "File Browser (current buffer)",
       },
+      -- find
+      { "fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", desc = "Find All Files" },
+      { "fc", LazyVim.telescope.config_files(), desc = "Find Config File" },
+      { "ff", LazyVim.telescope("files"), desc = "Find Files (Root Dir)" },
+      { "fF", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+      { "fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+      { "fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      { "fR", LazyVim.telescope("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+      { "fk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "fh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "fw", LazyVim.telescope("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
+      { "fW", LazyVim.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
+    },
+    opts = {
+      -- from NVChad:https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/telescope.lua
+      pickers = {
+        find_files = {
+          follow = true,
+        },
+      },
+
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "-L",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+
+        -- prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+
+        preview = {
+          filesize_limit = 1, -- MB
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+        mappings = {
+          n = {
+            ["q"] = require("telescope.actions").close,
+            ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+          i = {
+            ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+        },
+      },
     },
     config = function(_, opts)
       local telescope = require("telescope")
       local actions = require("telescope.actions")
       local fb_actions = require("telescope").extensions.file_browser.actions
-      local action_layout = require("telescope.actions.layout")
       local function telescope_buffer_dir()
         return vim.fn.expand("%:p:h")
       end
 
-      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-        file_ignore_patterns = {
-          "node_modules",
-        },
-        wrap_result = true,
-        preview = {
-          filesize_limit = 1, -- MB
-        },
-        mappings = {
-          n = {
-            ["<M-p>"] = action_layout.toggle_preview,
-          },
-          i = {
-            ["<M-p>"] = action_layout.toggle_preview,
-          },
-        },
-      })
-
-      opts.pickers = {
-        find_files = {
-          follow = true,
-        },
-      }
-
+      -- use file browser for telescope
       opts.extensions = {
         file_browser = {
           path = "%:p:h",
@@ -78,9 +134,23 @@ return {
           grouped = true,
           previewer = false,
           initial_mode = "normal",
-          layout_config = { height = 0.9 },
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+            },
+            vertical = {
+              mirror = false,
+            },
+            height = 0.80,
+            preview_cutoff = 120,
+          },
           theme = "dropdown",
           hijack_netrw = true,
+          border = {},
+          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
           mappings = {
             ["n"] = {
               -- override default function for open in built-in application
@@ -125,7 +195,6 @@ return {
         },
       }
       telescope.setup(opts)
-      require("telescope").load_extension("fzf")
       require("telescope").load_extension("file_browser")
     end,
   },
