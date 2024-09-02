34590bcb99ed6751f25f245246aad0c7bbdb09f8
Apollo-Dev <dangdoan2206@gmail.com>
2024-09-02 16:14:43 +0700

    update: add solarized

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,127 +1,194 @@
-return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "tokyonight",
-      -- colorscheme = "catppuccin",
-      -- colorscheme = "gruvbox",
-      colorscheme = "solarized-osaka",
-      -- colorscheme = "kanagawa",
-      -- colorscheme = "rose-pine",
-    },
-  },
+-- available configured colorschemes:
+-- "tokyonight", "catppuccin", "solarized-osaka", "solarized", "gruvbox", "kanagawa", "rose-pine"
+local colorscheme = "solarized-osaka" -- <- set colorscheme here
 
-  {
-    "folke/tokyonight.nvim",
-    enabled = false,
-    opts = {
-      style = "storm",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      -- custom line number color
-      on_highlights = function(highlights, colors)
-        highlights.LineNr = { fg = colors.yellow }
-        highlights.CursorLineNr = { fg = colors.green }
-      end,
-    },
+local lazyvim = {
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = colorscheme,
   },
+}
 
-  {
-    "catppuccin/nvim",
-    enabled = false,
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      flavour = "macchiato",
-      transparent_background = true,
-      -- custom line number color
-      custom_highlights = function(colors)
-        return {
-          CursorLineNr = { fg = colors.rosewater },
-          LineNr = { fg = colors.mauve },
-        }
-      end,
-    },
-  },
-
-  {
-    "craftzdog/solarized-osaka.nvim",
-    -- enabled = false,
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+if colorscheme == "tokyonight" then
+  return {
+    lazyvim,
+    {
+      "folke/tokyonight.nvim",
+      opts = {
+        style = "storm",
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        -- custom line number color
+        on_highlights = function(highlights, colors)
+          highlights.LineNr = { fg = colors.yellow }
+          highlights.CursorLineNr = { fg = colors.green }
+        end,
       },
     },
-  },
-
-  {
-    "ellisonleao/gruvbox.nvim",
-    enabled = false,
-    priority = 1000,
-    config = true,
-    opts = {
-      transparent_mode = true,
-    },
-  },
-
-  {
-    "rebelot/kanagawa.nvim",
-    enabled = false,
-    opts = {
-      transparent = true,
-      theme = "dragon",
-      background = {
-        dark = "dragon",
-        light = "lotus",
+  }
+elseif colorscheme == "catppuccin" then
+  return {
+    lazyvim,
+    {
+      "catppuccin/nvim",
+      lazy = true,
+      name = "catppuccin",
+      opts = {
+        flavour = "macchiato",
+        transparent_background = true,
+        -- custom line number color
+        custom_highlights = function(colors)
+          return {
+            CursorLineNr = { fg = colors.rosewater },
+            LineNr = { fg = colors.mauve },
+          }
+        end,
       },
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
+    },
+  }
+elseif colorscheme == "solarized-osaka" then
+  return {
+    lazyvim,
+    {
+      "craftzdog/solarized-osaka.nvim",
+      -- enabled = false,
+      lazy = false,
+      priority = 1000,
+      opts = {
+        -- transparent = false,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      },
+    },
+  }
+elseif colorscheme == "solarized" then
+  return {
+    lazyvim,
+    {
+      "maxmx03/solarized.nvim",
+      opts = {
+        transparent = {
+          enabled = true,
+        },
+        -- palette = "selenized",
+        variant = "autumn",
+        styles = {
+          comments = { italic = true, bold = false },
+          functions = { italic = true },
+        },
+      },
+    },
+  }
+elseif colorscheme == "gruvbox" then
+  return {
+    lazyvim,
+    {
+      "ellisonleao/gruvbox.nvim",
+      -- enabled = false,
+      priority = 1000,
+      config = true,
+      opts = {
+        transparent_mode = true,
+      },
+    },
+
+    {
+      "rebelot/kanagawa.nvim",
+      -- enabled = false,
+      opts = {
+        transparent = true,
+        theme = "dragon",
+        background = {
+          dark = "dragon",
+          light = "lotus",
+        },
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
             },
           },
         },
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-          TelescopeBorder = { bg = "none" },
-        }
-      end,
-    },
-  },
-
-  {
-    "rose-pine/neovim",
-    enabled = false,
-    name = "rose-pine",
-    opts = {
-      dark_variant = "moon",
-      styles = {
-        transparency = true,
-      },
-      highlight_groups = {
-        LineNr = { fg = "iris" },
-        CursorLineNr = { fg = "rose" },
-        TelescopeBorder = { fg = "highlight_high", bg = "none" },
-        TelescopeNormal = { bg = "none" },
-        TelescopePromptNormal = { bg = "none" },
-        TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-        TelescopeSelection = { fg = "text", bg = "base" },
-        TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            TelescopeBorder = { bg = "none" },
+          }
+        end,
       },
     },
-  },
-}
+  }
+elseif colorscheme == "kanagawa" then
+  return {
+    lazyvim,
+    {
+      "rebelot/kanagawa.nvim",
+      -- enabled = false,
+      opts = {
+        transparent = true,
+        theme = "dragon",
+        background = {
+          dark = "dragon",
+          light = "lotus",
+        },
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            TelescopeBorder = { bg = "none" },
+          }
+        end,
+      },
+    },
+  }
+elseif colorscheme == "rose-pine" then
+  return {
+    lazyvim,
+    {
+      "rose-pine/neovim",
+      -- enabled = false,
+      name = "rose-pine",
+      opts = {
+        dark_variant = "moon",
+        styles = {
+          transparency = true,
+        },
+        highlight_groups = {
+          LineNr = { fg = "iris" },
+          CursorLineNr = { fg = "rose" },
+          TelescopeBorder = { fg = "highlight_high", bg = "none" },
+          TelescopeNormal = { bg = "none" },
+          TelescopePromptNormal = { bg = "none" },
+          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+        },
+      },
+    },
+  }
+else
+  LazyVim.warn("Unknown colorscheme! Use default Tokyonight", { title = "Colorscheme 🌈" })
+  return {}
+end
