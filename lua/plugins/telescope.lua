db252a776c442e638eb24fa6a68bbbc32e49f33d
Apollo-Dev <dangdoan2206@gmail.com>
2024-09-03 18:39:52 +0700

    fix: merge custom config to default

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -28,6 +28,8 @@ return {
     -- short binding find
     { "fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>", desc = "Find All Files" },
     { "fc", LazyVim.pick.config_files(), desc = "Find Config File" },
+    { "fd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+    { "fD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
     { "ff", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
     { "fF", LazyVim.pick("auto", { root = false }), desc = "Find Files (cwd)" },
     { "fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
@@ -38,96 +40,92 @@ return {
     { "fw", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
     { "fW", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
   },
-  opts = function()
+  opts = {
+    pickers = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+      },
+      find_files = {
+        follow = true,
+      },
+    },
+    defaults = {
+      wrap_result = true,
+      winblend = 0,
+      prompt_prefix = "   ",
+      sorting_strategy = "ascending",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+        },
+        width = 0.87,
+        height = 0.80,
+      },
+
+      preview = {
+        filesize_limit = 10, -- MB
+      },
+    },
+  },
+  config = function(_, opts)
     local actions = require("telescope.actions")
     local fb_actions = require("telescope").extensions.file_browser.actions
     local function telescope_buffer_dir()
       return vim.fn.expand("%:p:h")
     end
-    return {
-      pickers = {
-        find_files = {
-          follow = true,
-        },
+    opts.mappings = {
+      n = {
+        ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
       },
-
-      defaults = {
-        wrap_result = true,
-        winblend = 0,
-        prompt_prefix = "   ",
-        selection_caret = " ",
-        entry_prefix = " ",
-        sorting_strategy = "ascending",
+      i = {
+        ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+      },
+    }
+    opts.extensions = {
+      file_browser = {
+        path = "%:p:h",
+        cwd = telescope_buffer_dir(),
+        respect_gitignore = false,
+        hidden = true,
+        follow_symlinks = true,
+        grouped = true,
+        previewer = false,
+        theme = "dropdown",
+        initial_mode = "normal",
+        hijack_netrw = true,
+        layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
             prompt_position = "top",
             preview_width = 0.55,
           },
-          width = 0.87,
           height = 0.80,
+          preview_cutoff = 120,
         },
-
-        preview = {
-          filesize_limit = 10, -- MB
-        },
-
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
         mappings = {
-          n = {
-            ["q"] = require("telescope.actions").close,
-            ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
-          },
-          i = {
-            ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
-          },
-        },
-      },
-
-      extensions = {
-        file_browser = {
-          path = "%:p:h",
-          cwd = telescope_buffer_dir(),
-          respect_gitignore = false,
-          hidden = true,
-          follow_symlinks = true,
-          grouped = true,
-          previewer = false,
-          theme = "dropdown",
-          initial_mode = "normal",
-          hijack_netrw = true,
-          layout_strategy = "horizontal",
-          layout_config = {
-            horizontal = {
-              prompt_position = "top",
-              preview_width = 0.55,
-            },
-            height = 0.80,
-            preview_cutoff = 120,
-          },
-          border = {},
-          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          mappings = {
-            ["n"] = {
-              ["h"] = fb_actions.goto_parent_dir,
-              ["<bs>"] = fb_actions.goto_parent_dir,
-              ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 5 do
-                  actions.move_selection_previous(prompt_bufnr)
-                end
-              end,
-              ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 5 do
-                  actions.move_selection_next(prompt_bufnr)
-                end
-              end,
-              ["<C-f>"] = actions.preview_scrolling_down,
-              ["<C-b>"] = actions.preview_scrolling_up,
-            },
+          ["n"] = {
+            ["h"] = fb_actions.goto_parent_dir,
+            ["<bs>"] = fb_actions.goto_parent_dir,
+            ["<C-u>"] = function(prompt_bufnr)
+              for i = 1, 5 do
+                actions.move_selection_previous(prompt_bufnr)
+              end
+            end,
+            ["<C-d>"] = function(prompt_bufnr)
+              for i = 1, 5 do
+                actions.move_selection_next(prompt_bufnr)
+              end
+            end,
+            ["<C-f>"] = actions.preview_scrolling_down,
+            ["<C-b>"] = actions.preview_scrolling_up,
           },
         },
       },
     }
-  end,
-  config = function(_, opts)
     require("telescope").setup(opts)
     require("telescope").load_extension("file_browser")
   end,
